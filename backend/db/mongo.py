5bcfc0ffa502533e44da5abefd7284023964f8f3
Manikandan M P <manikandanmperikamana@gmail.com>
2026-03-28 13:20:06 +0530

    add MongoDB service layer with async connection management and db accessor

--- backend/db/mongo.py
+++ backend/db/mongo.py
@@ -1,1 +1,22 @@
-﻿
+﻿from motor.motor_asyncio import AsyncIOMotorClient
+from dotenv import load_dotenv
+import os
+
+load_dotenv()
+
+class MongoDB:
+    client: AsyncIOMotorClient = None
+    db = None
+
+db_instance = MongoDB()
+
+async def connect_db():
+    db_instance.client = AsyncIOMotorClient(os.getenv("MONGODB_URL"))
+    db_instance.db = db_instance.client[os.getenv("DB_NAME")]
+    print("✅ MongoDB connected")
+
+async def disconnect_db():
+    db_instance.client.close()
+
+def get_db():
+    return db_instance.db
