7864f5bfa0e88a69abe67fdeeb1fe290b48741e5
Manikandan M P <manikandanmperikamana@gmail.com>
2026-03-28 15:32:07 +0530

    Changed LLMto gemini model
    Used gemini api key

--- backend/services/openai_service.py
+++ backend/services/openai_service.py
@@ -1,21 +1,35 @@
-﻿from openai import AsyncOpenAI
-from dotenv import load_dotenv
+﻿from dotenv import load_dotenv
+import google.generativeai as genai
+from pathlib import Path
 import os
+import json
 
 load_dotenv()
-client = AsyncOpenAI(api_key=os.getenv("OPENAI_API_KEY"))
+# load_dotenv(Path(__file__).parent.parent / ".env")
+print("KEY EXISTS:", "GOOGLE_API_KEY" in os.environ)
+genai.configure(api_key=os.getenv("GOOGLE_API_KEY"))
+
 
 async def call_llm(system_prompt: str, user_message: str, json_mode: bool = False) -> str:
-    kwargs = {
-        "model": "gpt-4o",
-        "messages": [
-            {"role": "system", "content": system_prompt},
-            {"role": "user", "content": user_message}
-        ],
-        "temperature": 0.3,
-    }
+    model = genai.GenerativeModel(
+        model_name="gemini-2.5-flash",
+        system_instruction=system_prompt
+    )
+
     if json_mode:
-        kwargs["response_format"] = {"type": "json_object"}
-    
-    response = await client.chat.completions.create(**kwargs)
-    return response.choices[0].message.content
+        response = model.generate_content(
+            user_message,
+            generation_config=genai.GenerationConfig(
+                response_mime_type="application/json",
+                temperature=0.3
+            )
+        )
+    else:
+        response = model.generate_content(
+            user_message,
+            generation_config=genai.GenerationConfig(
+                temperature=0.3
+            )
+        )
+
+    return response.text
